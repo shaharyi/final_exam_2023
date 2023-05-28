cff53fae6603b51933c89babaaac0968f99a4000
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-05-28 16:08:14 +0300

    Update Main1.java

--- src/main/q1/Main1.java
+++ src/main/q1/Main1.java
@@ -2,8 +2,9 @@
 public class Main1 {
 	/*
 	 * No duplicates in both a, b.
+	 * Return "Is a contained in b in order?"
 	 */
-	public static boolean isOrderlySubset(char[] a, char[] b) {
+	public static boolean isOrderlyContained(char[] a, char[] b) {
 		int i = 0;
 		for (int j = 0; j < b.length; j++)
 			if (i < a.length && a[i] == b[j])
