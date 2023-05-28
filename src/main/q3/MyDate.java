b3cf7a4ffb9a9cc704555cdb0fd49b149196051f
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-05-28 16:21:02 +0300

    Create MyDate.java

---
+++ src/main/q3/MyDate.java
@@ -0,0 +1,17 @@
+
+/*
+ * This file is only for your convenience.
+ */
+public class MyDate {
+	private int day;
+	private int month;
+	private int year;
+	
+	public MyDate(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+
+	public int diffDays (MyDate date) { return -1; }
+}
