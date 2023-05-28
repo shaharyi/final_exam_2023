daae4676c06c9ef1123b192bbce1f0a065357dad
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2023-05-28 16:06:56 +0300

    Update Main1.java

--- src/main/q1/Main1.java
+++ src/main/q1/Main1.java
@@ -1,7 +1,9 @@
 
 public class Main1 {
-
-	public static boolean isSubset(char[] a, char[] b) {
+	/*
+	 * No duplicates in both a, b.
+	 */
+	public static boolean isOrderlySubset(char[] a, char[] b) {
 		int i = 0;
 		for (int j = 0; j < b.length; j++)
 			if (i < a.length && a[i] == b[j])
